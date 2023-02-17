a9f1d74f90246c1fb87b34416f0210ae6434f1df
Shivakumar.D <107037061+shivakumard17@users.noreply.github.com>
2023-02-17 11:51:23 +0530

    Update All.jsx

--- src/pages/All.jsx
+++ src/pages/All.jsx
@@ -11,7 +11,7 @@ function All() {
 
     useEffect(() => {
         const getArticles = async () => {
-            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=f0d1eff0cd78421eb8f6223510e8ee56`)
+            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=004551c8b9734e6992841149e222c96a`)
             setArticles(response.data.articles);
             console.log(response);
         }
