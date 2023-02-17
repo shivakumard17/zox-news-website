065ddf83a1e56199049ee14d024dcdeda417ebd3
Shivakumar.D <107037061+shivakumard17@users.noreply.github.com>
2023-02-17 11:51:44 +0530

    Update Bussiness.jsx

--- src/pages/Bussiness.jsx
+++ src/pages/Bussiness.jsx
@@ -9,7 +9,7 @@ function Bussiness() {
 
     useEffect(() => {
         const getArticles = async () => {
-            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f0d1eff0cd78421eb8f6223510e8ee56`)
+            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=004551c8b9734e6992841149e222c96a`)
             setArticles(response.data.articles);
             console.log(response);
         }
